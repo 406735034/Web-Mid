724e65aff24475ae60f73a3748db39dfb260da0d
406735034 <tvbchary@gmail.com>
2019-11-11 12:43:43 +0800

    Updated

--- p2/div_id.js
+++ p2/div_id.js
@@ -1,13 +1,28 @@
-function changeImage(index){
-const p= document.getElementById('section')
-console.log(p)
-
+function changeStyle(index) {
+const p = document.getElementById('section');
 switch(index) {
-
-    case 1: p.innerHTML='<img src="img/img1.jpg" width=40%/>';'<img src="img/img2.jpg" width=40%/>';
+    case 1:
+      p.innerHTML = '<img src="img/img1.jpg" width=40% /><img src="img/img2.jpg" width = 40%/>';
     break;
-    
-
+    case 2:
+      p.innerHTML = '<img src="img/img2.jpg" width=40% /><img src="img/img3.jpg" width = 40%/>';
+    break;
+    case 3:
+      p.innerHTML = '<img src="img/img3.jpg" width=40% /><img src="img/img4.jpg" width = 40%/>';
+    break;
+    case 4:
+      p.innerHTML = '<img src="img/img4.jpg" width=40% /><img src="img/img5.jpg" width = 40%/>';
+    break;
+    case 5:
+      p.innerHTML = '<img src="img/img5.jpg" width=40% /><img src="img/img6.jpg" width = 40%/>';
+    break;
+    case 6:
+      p.innerHTML = '<img src="img/img6.jpg" width=40% /><img src="img/img7.jpg" width = 40%/>';
+    break;
+    case 7:
+      p.innerHTML = '<img src="img/img7.jpg" width=40% /><img src="img/img8.jpg" width = 40%/>';
+    break;
+    }         
 }
 
-}
+
