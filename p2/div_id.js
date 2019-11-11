0ecdd53e8e5a277f95bc954078f815097d007b4b
406735034 <tvbchary@gms.tku.edu.tw>
2019-11-11 12:36:46 +0800

    All are Ok

---
+++ p2/div_id.js
@@ -0,0 +1,13 @@
+function changeImage(index){
+const p= document.getElementById('section')
+console.log(p)
+
+switch(index) {
+
+    case 1: p.innerHTML='<img src="img/img1.jpg" width=40%/>';'<img src="img/img2.jpg" width=40%/>';
+    break;
+    
+
+}
+
+}
